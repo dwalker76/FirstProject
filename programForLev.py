f99308bae4045dcb2a76263d697e456a6b69c33b
Ged <53939907+Ged-sudo@users.noreply.github.com>
2022-02-22 19:08:44 +0300

    Update programForLev.py
    так удобней)

--- programForLev.py
+++ programForLev.py
@@ -2,15 +2,16 @@ import math
 
 a = [0,2,5,1,2,5,8,2]
 
-for i in range(8):
-    for j in range(7):
+for i in range(len(a)):
+    for j in range(len(a) - 1):
         if (a[i] > a[j]):
-            c = a[i]
-            a[i] = a[j]
-            a[j] = c
-
+            #c = a[i]
+            #a[i] = a[j]
+            #a[j] = c
+            a[i],a[j] = a[j],a[i]
+            
 print(a)
-for i in range(8):
+for i in range(len(a)):
     print(a[i])
     
 #LevPrivet =)
